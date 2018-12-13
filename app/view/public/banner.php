2629f5461f088de7f444575b77fa87c7f9edb0f3
Douglas Mesquita <douglasalipio@gmail.com>
2018-12-13 19:14:05 +0000

    another template

---
+++ app/view/public/banner.php
@@ -0,0 +1,11 @@
+<!-- PUBLIC MENU -->
+
+    <!-- MENU -->
+    <nav class="navbar navbar-expand-sm navbar-dark bg-primary fixed-top">
+
+      <a class="navbar-brand" href="index.php">
+        <img src="img/robot.svg" alt="robot logo" style="width:60px;">
+      </a> 
+        
+    </nav>           
+        
