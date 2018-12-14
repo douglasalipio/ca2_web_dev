a1d262de2a6361d4dc2a46d0a7bbb7050d31ee36
Douglas Mesquita <douglasalipio@gmail.com>
2018-12-14 12:13:12 +0000

    format home page

--- home.php
+++ home.php
@@ -1,148 +1,114 @@
 <!DOCTYPE html>
-
 <?php
-
 // PRIVATE PAGE
-
 //needs to be at the start of every page where you will use $_SESSION
 session_start();
-
 if (!isset($_SESSION["loggedIn"]) && !$_SESSION["loggedIn"] == 1)
 {
-    //throw you off the page if not logged in
-    header("Location:index.php");
+//throw you off the page if not logged in
+header("Location:index.php");
 }
-
 $menuActive_home = "active";
 define("MY_INC_CODE", 888);
 define("APPLICATION_PATH", "app");
 define("VIEW_PATH", APPLICATION_PATH . "/view");
 define("MODEL_PATH", APPLICATION_PATH . "/model");
 include (APPLICATION_PATH . "/inc/config.inc.php");
-
 ?>
-
 <html lang="en">
-    
-<head>
-    
-<?php include (VIEW_PATH . "/head.php"); ?>
-    
-</head>
-
-<body>
-        
-<!-- HEADER ------------------------------------------------>
-        
-<?php 
-    echo "<header>";
-    include (VIEW_PATH . "/private/navigation.php"); 
-    echo "</header>";
-    
-    include (APPLICATION_PATH . "/inc/db.inc.php");  
-?>
-        
-<!-- MAIN CONTENT ------------------------------------------>
-<main class="margin-top-6">  
-<div class="container">
-
-<!-- .................................................... -->      
-<section id="featured">
-      
-<!-- ROW 1 -->
-<div class="row">
-<div class="col-sm-12">
-
-<section id="theTop">
-    
-      <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
-  <div class="carousel-inner">
-    <div class="carousel-item active">
-      <img class="d-block w-100" src="img/capoeira1.jpg" alt="First slide">
-    </div>
-    <div class="carousel-item">
-      <img class="d-block w-100" src="img/capoeira_stencil.jpg" alt="Second slide">
-    </div>
-    <div class="carousel-item">
-      <img class="d-block w-100" src="img/capoeira3.jpg" alt="Third slide">
-    </div>
-  </div>
-  <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="sr-only">Previous</span>
-  </a>
-  <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="sr-only">Next</span>
-  </a>
-</div>
-      <!-- ROW 1 -->
-      <div class="row">
-      <div class="col-sm-12" >
-
-        
-
-      </div>
-        </div>
-          <div class="row margin-top-3">
-            <div class="col-sm-4">
-                <h4>Contrary to popular belief, Lorem Ipsum is not simply?</h4>
-                <p>dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-
-            </div>
-            <div class="col-sm-4">
-                <h4>Pellentesque adipiscing commodo elit at?</h4>
-                <p>dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum..</p>
-            </div>
-            <div class="col-sm-4">
-              <div class="embed-responsive embed-responsive-16by9">
-                <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/pzAJKaDoew8" allowfullscreen></iframe>
-              </div>
-          </div>
-      </div>                      
-</section>      
-
-<form action="submitSubscription.php?" method="POST">
-  <div class="container">
-    <h2>Subscribe to our Newsletter</h2>
-    <p>Lorem ipsum text about why you should subscribe to our newsletter blabla. Lorem ipsum text about why you should subscribe to our newsletter blabla.</p>
-  </div>
-
-  <div class="container" style="background-color:white">
-    <input id="name" type="text" placeholder="Name" name="name" required>
-    <input id="email" type="text" placeholder="Email address" name="mail" required>
-    <label>
-      <input type="checkbox" checked="checked" name="subscribe"> Daily Newsletter
-    </label>
-  </div>
-
-  <div class="container">
-    <input type="submit" value="Subscribe">
-  </div>
-</form>
-</div>     
-
-    
-</div><!-- column -->
-</div><!-- row -->
-<!-- END ROW 1 -->      
-
-            
-</section>      
-<!-- .................................................... -->
-
-</div> <!-- END container-->    
-</main>
-
-<!-- FOOTER ------------------------------------------------>
-<footer>
-    <?php include (VIEW_PATH . "/private/footer.php"); ?>
-</footer>
-        
-    
-<!-- all content above this line -->    
-<?php include (VIEW_PATH . "/foot.php"); ?>
-        
+	
+	<head>
+		
+		<?php include (VIEW_PATH . "/head.php"); ?>
+		
+	</head>
+	<body>
+		
+		<!-- HEADER ------------------------------------------------>
+		
+		<?php
+		echo "<header>";
+												include (VIEW_PATH . "/private/navigation.php");
+		echo "</header>";
+		
+		include (APPLICATION_PATH . "/inc/db.inc.php");
+		?>
+		<main class="margin-top-6">
+			<div class="container">
+				<section id="featured">
+					<div class="row">
+						<div class="col-sm-12">
+							<section id="theTop">
+								
+								<div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+									<div class="carousel-inner">
+										<div class="carousel-item active">
+											<img class="d-block w-100" src="img/capoeira1.jpg" alt="First slide">
+										</div>
+										<div class="carousel-item">
+											<img class="d-block w-100" src="img/capoeira_stencil.jpg" alt="Second slide">
+										</div>
+										<div class="carousel-item">
+											<img class="d-block w-100" src="img/capoeira3.jpg" alt="Third slide">
+										</div>
+									</div>
+									<a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+										<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+										<span class="sr-only">Previous</span>
+									</a>
+									<a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+										<span class="carousel-control-next-icon" aria-hidden="true"></span>
+										<span class="sr-only">Next</span>
+									</a>
+								</div>
+								<div class="row">
+									<div class="col-sm-12">
+									</div>
+								</div>
+								<div class="row margin-top-3">
+									<div class="col-sm-4">
+										<h4>Contrary to popular belief, Lorem Ipsum is not simply?</h4>
+										<p>dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+									</div>
+									<div class="col-sm-4">
+										<h4>Pellentesque adipiscing commodo elit at?</h4>
+										<p>dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum..</p>
+									</div>
+									<div class="col-sm-4">
+										<div class="embed-responsive embed-responsive-16by9">
+											<iframe class="embed-responsive-item" src="https://www.youtube.com/embed/pzAJKaDoew8" allowfullscreen></iframe>
+										</div>
+									</div>
+								</div>
+							</section>
+							
+							<form action="submitSubscription.php?" method="POST">
+								<div class="container">
+									<h2>Subscribe to our Newsletter</h2>
+									<p>Lorem ipsum text about why you should subscribe to our newsletter blabla. Lorem ipsum text about why you should subscribe to our newsletter blabla.</p>
+								</div>
+								<div class="container" style="background-color:white">
+									<input id="name" type="text" placeholder="Name" name="name" required>
+									<input id="email" type="text" placeholder="Email address" name="mail" required>
+									<label>
+										<input type="checkbox" checked="checked" name="subscribe"> Daily Newsletter
+									</label>
+								</div>
+								
+								<div class="container">
+									<input type="submit" value="Subscribe">
+								</div>
+							</form>
+						</div>
+						
+					</div>
+				</div>
+			</section>
+		</div>
+	</main>
+	<footer>
+		<?php include (VIEW_PATH . "/private/footer.php"); ?>
+	</footer>
+	<?php include (VIEW_PATH . "/foot.php"); ?>
 </body>
 </html>
-
