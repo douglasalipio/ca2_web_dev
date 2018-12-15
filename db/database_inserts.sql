7da5768816cb435570e1e8193aa855ac11d66484
Douglas Mesquita <douglasalipio@gmail.com>
2018-12-15 17:10:49 +0000

    remove images

--- db/database_inserts.sql
+++ db/database_inserts.sql
@@ -1,11 +1,7 @@
 BEGIN TRANSACTION;
-insert into Robots (Robot_ID, RobotName, RobotDescription, RobotCost, RobotImage) values  ('ROB001', 'Ultron', 'Evil take over the world android from the Marvel universe', '6000000', 'ultron.jpg');
-insert into Robots (Robot_ID, RobotName, RobotDescription, RobotCost, RobotImage) values  ('ROB002', 'R2D2', 'Star Wars good guy', '567770', 'R2D2.jpeg');
-insert into Robots (Robot_ID, RobotName, RobotDescription, RobotCost, RobotImage) values  ('ROB003', 'Optimus Prime', 'Leader of the transformers', '70098000', 'prime.jpeg');
-insert into Robots (Robot_ID, RobotName, RobotDescription, RobotCost, RobotImage) values  ('ROB004', 'Wally', 'Disney robot left on earth to clean it up', '6000000', 'Wally.jpeg');
-insert into Robots (Robot_ID, RobotName, RobotDescription, RobotCost, RobotImage) values  ('ROB005', 'Fender', 'Futurama robot - only cares about himself, most of the time', '0.1', 'fender.jpg');
-COMMIT;
 
-BEGIN TRANSACTION;
-insert into User (user_name, user_password, user_password_hash, user_email ) values  ('Ultron', 'evilone', '5555', 'ultron@evilones.com');
+insert into User (user_name, user_password, user_password_hash, user_email ) 
+values  ('Ultron', 'evilone', '5555', 'ultron@evilones.com'), ('João', 'pass123!#', '5555', 'joao@evilla.com'), 
+('Douglas', 'pwd', '5555', 'douglas@gmail.com');
+
 COMMIT;
